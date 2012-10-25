91a744525a8ec134731bb4a77a8c0f50c80c39e8
Leandro Sales Pinto <leandro.shp@gmail.com>
2012-10-25 17:37:46 -0400

    v2 changes

--- src/main/java/org/testevol/Math.java
+++ src/main/java/org/testevol/Math.java
@@ -2,12 +2,14 @@ package org.testevol;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.io.InputStreamReader;
+
+import org.testevol.dumb.DumbObj;
 
 public class Math {
-
+	
 	public static int round(double a){
-		if(a + 0.5 > (int)(a+1)) return (int)(a + 1);
+		if(a + 0.5 >= (int)(a+1)) return (int)(a + 1);
 		
 		return (int)a;
 	}
@@ -26,7 +28,7 @@ public class Math {
 		return result;
 	}
 
-	public static int abs(int value){
+	public static double abs(double value){
 		if(value < 0){
 			return -1*value;
 		}
@@ -34,22 +36,30 @@ public class Math {
 		return value;
 	}
 	
-	public static int max(int a, int b){
+	public static double max(double a, double b){
 		if(a > b) return a;
 		return b;
 	}
-
+	
 	public static int min(int a, int b){
+		if(a == b) throw new RuntimeException("a cannot be equal to b, too easy!");
 		if(a < b) return a;
 		return b;
 	}
-
+	
 	public static double tan(Double a){
+		if(a == null){
+			a = 0d;
+		}
 		return java.lang.Math.tan(a.doubleValue());
 	}
 	
-	public static int sumFile(Reader reader) throws IOException{
-		BufferedReader fileReader = new BufferedReader(reader);
+	public static double sqrt(double a){
+		return java.lang.Math.sqrt(a);
+	}
+
+	public static int sumFile() throws IOException{
+		BufferedReader fileReader = new BufferedReader(new InputStreamReader(Math.class.getResourceAsStream("test.txt")));
 		int sum = 0;
 		String line;
 		while((line = fileReader.readLine()) != null){
@@ -62,7 +72,7 @@ public class Math {
 		System.out.println(s);
 		return s;
 	}
-	
+
 	public static String echo(String s){
 		System.out.println(s);
 		return s;
---
+++ src/main/java/org/testevol/dumb/DumbObj.java
@@ -0,0 +1,11 @@
+package org.testevol.dumb;
+
+public class DumbObj {
+
+	public DumbObj(String value) {
+		this.value = value;
+	}
+	
+	public String value;
+	
+}
